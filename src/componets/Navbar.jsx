045efca4cad65bad0d9e149a7a84f6804604a594
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2025-12-21 17:59:40 +0530

    create recipes page added

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center text-sm gap-x-10">
+    <div className="flex justify-center items-center text-sm gap-x-10 mb-10">
         
-        <NavLink className={(e) => e.isActive && "text-red-300"} to="/">Home</NavLink>
-        <NavLink className={(e) => e.isActive && "text-red-300"} to="/recipes">Recipes</NavLink>
-
+        <NavLink className={(e) => e.isActive ? "text-red-300" :""} to="/">Home</NavLink>
+        <NavLink className={(e) => e.isActive ? "text-red-300" :""} to="/recipes">Recipes</NavLink>
+        <NavLink className={(e) => e.isActive ? "text-red-300" :""} to="/about">About</NavLink>
+        <NavLink className={(e) => e.isActive ? "text-red-300" :""} to="/CreateRecipe">Create Recipe</NavLink>
 
     </div>
   )
--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -2,6 +2,8 @@ import {Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Recipes from '../pages/Recipes';
+import Create from '../pages/Create';
+
 
 
 const Mainroutes = () => {
@@ -10,6 +12,7 @@ const Mainroutes = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/recipes' element={<Recipes/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/CreateRecipe' element={<Create/>}/>
     </Routes>
   );
 }
