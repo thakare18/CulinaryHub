e1f84fa8539844d19fe8b48bf53931b7d750c88c
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-01 00:17:31 +0530

    Recipes page to display a list of recipes

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { recipecontext } from "../context/RecipeContext";
+import RecipeCard from "../components/RecipeCard";
 
 const Recipes = () => {
-  return (
-    <div>
-        Recipes Page
-    </div>
-  )
-}
+  const { data } = useContext(recipecontext);
 
-export default Recipes
+  const renderrecipes = data.map((recipe) => (
+    <RecipeCard key={recipe.id} recipe={recipe} />
+  ));
+
+  return (
+    <div className="grid gap-6 px-10 py-10 text-black  grid-cols-1  md:grid-cols-3 lg:grid-cols-4">
+      {" "}
+      {data.length > 0 ? (
+        renderrecipes
+      ) : (
+        <div className="w-full flex justify-center items-center h-[50vh] col-span-full">
+          <p className="text-2xl font-semibold text-gray-400">
+            No recipes found!
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Recipes;
