807e794076212aa04cdc9b46c3e67298f5fbe54a
Prathamesh Vinayak Thakare <prathamthackeray18@gmail.com>
2026-01-01 00:14:42 +0530

    Added Page Not Found for handling 404 errors

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="w-full flex justify-center items-center h-[50vh]">
+      <p className="text-2xl font-semibold text-gray-400">The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
